bc63223ef750a1e1530f9f8843e44428958a7723
0xcyborg <104452787+0xcyborg@users.noreply.github.com>
2022-04-26 17:38:59 +0100

    Add files via upload

---
+++ Tool.py
@@ -0,0 +1,18 @@
+# 2022-04-26
+
+from socket import gethostbyname
+
+with open("List.txt","r") as File:
+  Domains = File.readlines()
+
+for Domain in Domains:
+  try:
+    Domain = Domain.replace("*.","").strip()
+    gethostbyname(Domain)
+    with open("Working.txt","a") as File:
+      File.write(Domain + "\n")
+    print("Working: ", Domain)
+  except Exception:
+    print("Not Working: ", Domain)
+else:
+  print("Done All")
